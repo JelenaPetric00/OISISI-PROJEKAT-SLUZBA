e632121a60cb226a91b978d3e7f383dc1d94c59d
Jelena Petric <petric.ra183.2019@uns.ac.rs>
2021-11-05 16:46:26 +0100

    fixing pull problem part 2

--- src/main/MyApp.java
+++ src/main/MyApp.java
@@ -1,20 +1,10 @@
 package main;
 
-import javax.swing.SwingUtilities;
-
 public class MyApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		
-		SwingUtilities.invokeLater(new Runnable() {	//same thread for all of swing code 
-			@Override
-			public void run() {
-				MainWindow main = new MainWindow();
-				main.show();
-			}
-		});
 
 	}
 
