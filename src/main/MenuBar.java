c9c20b2988a7457707e41fca95c527f4e93f4102
Jelena Petric <petric.ra183.2019@uns.ac.rs>
2021-11-16 23:05:46 +0100

    Modified MenuBar

--- src/main/MenuBar.java
+++ src/main/MenuBar.java
@@ -1,11 +1,13 @@
 package main;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 public class MenuBar extends JMenuBar {
 	
@@ -24,14 +26,17 @@ public class MenuBar extends JMenuBar {
 		openItem.setIcon(openIcon);
 		
 		 JMenuItem miStudents = new JMenuItem("Studenti");
-		 ImageIcon optionIcon = new ImageIcon(new ImageIcon("icons/option.png").getImage().getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH));
-		 miStudents.setIcon(optionIcon);
+		 ImageIcon studentIcon = new ImageIcon(new ImageIcon("icons/student.png").getImage().getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH));
+		 miStudents.setIcon(studentIcon);
 		 JMenuItem miSubjects = new JMenuItem("Predmeti");
-		 miSubjects.setIcon(optionIcon);
+		 ImageIcon subjectIcon = new ImageIcon(new ImageIcon("icons/subject.png").getImage().getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH));
+		 miSubjects.setIcon(subjectIcon);
 		 JMenuItem miProfessors = new JMenuItem("Profesori");
-		 miProfessors.setIcon(optionIcon);
+		 ImageIcon professorIcon = new ImageIcon(new ImageIcon("icons/professor.png").getImage().getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH));
+		 miProfessors.setIcon(professorIcon);
 		 JMenuItem miDesks = new JMenuItem("Katedre");
-		 miDesks.setIcon(optionIcon);
+		 ImageIcon desksIcon = new ImageIcon(new ImageIcon("icons/desks.png").getImage().getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH));
+		 miDesks.setIcon(desksIcon);
 		 
 		JMenuItem closeItem = new JMenuItem("Close");
 		ImageIcon closeIcon = new ImageIcon(new ImageIcon("icons/close.png").getImage().getScaledInstance(15, 15, java.awt.Image.SCALE_SMOOTH));
@@ -96,6 +101,20 @@ public class MenuBar extends JMenuBar {
 		miSubjects.setMnemonic(KeyEvent.VK_P);   // Alt + p
 		miProfessors.setMnemonic(KeyEvent.VK_R); // Alt + r
 		miDesks.setMnemonic(KeyEvent.VK_K);      // Alt + k
+		
+		//Accelerator
+		newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+		saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+		closeItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+		editItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+		deleteItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
+		helpItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, ActionEvent.CTRL_MASK));
+		aboutItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+		
+		miStudents.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, ActionEvent.CTRL_MASK));
+		miSubjects.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+		miProfessors.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, ActionEvent.CTRL_MASK));
+		miDesks.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_K, ActionEvent.CTRL_MASK));
 	}
 
 }
