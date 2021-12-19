48eb93c5f8e6176f8e34b6a0966f3a60df5cde01
Isidora Poznanovic <poznanovic.ra163.2019@uns.acs.rs>
2021-12-19 17:59:58 +0100

    Fixed size of tables

--- src/view/tabs/StudentsTab.java
+++ src/view/tabs/StudentsTab.java
@@ -2,9 +2,11 @@ package view.tabs;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
 
 import view.tables.AbstractTableModelStudents;
 import view.tables.StudentsTable;
@@ -13,6 +15,7 @@ public class StudentsTab extends JPanel {
 	private StudentsTable studentsTable;
 	
 	private static StudentsTab instance;
+	private int heightRow = 40;
 	
 	public static StudentsTab getInstance() {
 		if (instance == null) {
@@ -25,7 +28,17 @@ public class StudentsTab extends JPanel {
 		this.setLayout(new BorderLayout());
 		studentsTable = StudentsTable.getInstance();
 		add(studentsTable);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		studentsTable.getColumnModel().getColumn(0).setPreferredWidth(screenSize.width*6/24);
+		studentsTable.getColumnModel().getColumn(1).setPreferredWidth(screenSize.width*5/24);
+		studentsTable.getColumnModel().getColumn(2).setPreferredWidth(screenSize.width*6/24);
+		studentsTable.getColumnModel().getColumn(3).setPreferredWidth(screenSize.width*3/24);
+		studentsTable.getColumnModel().getColumn(4).setPreferredWidth(screenSize.width*2/24);
+		studentsTable.getColumnModel().getColumn(5).setPreferredWidth(screenSize.width*3/24);
+		studentsTable.setRowHeight(heightRow);
+		studentsTable.setAutoResizeMode(StudentsTable.AUTO_RESIZE_LAST_COLUMN);
 		JScrollPane scrollPane = new JScrollPane(studentsTable);
+		scrollPane.setBorder(new EmptyBorder(25, 50, 25, 50));
 		add(scrollPane, BorderLayout.CENTER);
 
 		this.updateView(null, -1);
--- src/view/tabs/SubjectsTab.java
+++ src/view/tabs/SubjectsTab.java
@@ -1,17 +1,22 @@
 package view.tabs;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
 
 import view.tables.AbstractTableModelSubjects;
+import view.tables.StudentsTable;
 import view.tables.SubjectsTable;
 
 public class SubjectsTab extends JPanel{
 	private SubjectsTable subjectsTable;
 	
 	private static SubjectsTab instance;
+	private int heightRow = 40;
 	
 	public static SubjectsTab getInstance(){
 		if(instance == null){
@@ -24,8 +29,16 @@ public class SubjectsTab extends JPanel{
 		this.setLayout(new BorderLayout());
 		subjectsTable = SubjectsTable.getInstance();
 		add(subjectsTable);
-		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		subjectsTable.getColumnModel().getColumn(0).setPreferredWidth(screenSize.width*2/10);
+		subjectsTable.getColumnModel().getColumn(1).setPreferredWidth(screenSize.width*4/10);
+		subjectsTable.getColumnModel().getColumn(2).setPreferredWidth(screenSize.width*1/10);
+		subjectsTable.getColumnModel().getColumn(3).setPreferredWidth(screenSize.width*1/10);
+		subjectsTable.getColumnModel().getColumn(4).setPreferredWidth(screenSize.width*1/10);
+		subjectsTable.setRowHeight(heightRow);
+		subjectsTable.setAutoResizeMode(StudentsTable.AUTO_RESIZE_LAST_COLUMN);
 		JScrollPane scrollP = new JScrollPane(subjectsTable);
+		scrollP.setBorder(new EmptyBorder(25, 50, 25, 50));
 		add(scrollP, BorderLayout.CENTER);
 		
 		this.updateView(null, -1);
